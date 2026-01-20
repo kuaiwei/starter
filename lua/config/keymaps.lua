3508b6997fa3ed8fe616c2a87b6386c884ce1f16
Kuai Wei <kuaiwei.kw@alibaba-inc.com>
2026-01-20 13:22:45 +0800

    Basic setting and key mapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,37 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opt = {
+  noremap = true,
+  silent = true,
+}
+
+local map = vim.api.nvim_set_keymap
+-- move between windows
+map("n", "<A-h>", "<C-w>h", opt)
+map("n", "<A-j>", "<C-w>j", opt)
+map("n", "<A-k>", "<C-w>k", opt)
+map("n", "<A-l>", "<C-w>l", opt)
+
+-- toggle mouse/fold support, it help copy text in terminal
+local toggle_mouse_and_fold = function()
+  if (vim.opt.mouse._value == 'a') then
+    print("Disable mouse and fold")
+    vim.opt.mouse = ''
+    vim.opt.foldenable = false
+  else
+    print("Enable mouse and fold")
+    vim.opt.mouse = 'a'
+    vim.opt.foldenable = true
+  end
+end
+
+map("", "<F2>", '', {
+  noremap = true,
+  silent = true,
+  callback = function ()
+    toggle_mouse_and_fold()
+  end,
+  desc = "toggle both mouse mode and fold (enable/disable)"
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,33 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- basic option and setting of neovim
+local options = {
+  background    = "light",
+  backup        = false,
+  clipboard     = "unnamedplus",          -- allows neovim to access the system clipboard
+  hlsearch      = true,
+  ignorecase    = true,
+  mouse         = "",                     -- disable mouse by default, use <F2> to switch mouse support
+  pumheight     = 10,                     -- popup menu height
+  showtabline   = 2,                      -- always show tabs
+  smartcase     = true,
+  smartindent   = true,
+  splitright    = true,
+  splitbelow    = true,
+  swapfile      = false,
+  timeoutlen    = 300,                    -- time to wait for a mapped sequence to complete (in milliseconds)
+  writebackup   = false,
+  expandtab     = true,
+  shiftwidth    = 2,
+  tabstop       = 2,
+  wrap          = true,
+  linebreak     = true,
+  scrolloff     = 8,                      -- minimal number of screen lines to keep above and below the cursor
+  sidescrolloff = 8,                      -- minimal number of screen columns either side of cursor if wrap is `false`
+}
+
+for k,v in pairs(options) do
+  vim.opt[k] = v
+end

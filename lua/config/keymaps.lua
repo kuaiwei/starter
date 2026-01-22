70064080326942692f7890d9af3f50f989744203
Kuai Wei <kuaiwei.kw@alibaba-inc.com>
2026-01-22 15:48:14 +0800

    reset 'n' 'N' keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,22 +16,30 @@ map("n", "<A-l>", "<C-w>l", opt)
 
 -- toggle mouse/fold support, it help copy text in terminal
 local toggle_mouse_and_fold = function()
-  if (vim.opt.mouse._value == 'a') then
+  if vim.opt.mouse._value == "a" then
     print("Disable mouse and fold")
-    vim.opt.mouse = ''
+    vim.opt.mouse = ""
     vim.opt.foldenable = false
   else
     print("Enable mouse and fold")
-    vim.opt.mouse = 'a'
+    vim.opt.mouse = "a"
     vim.opt.foldenable = true
   end
 end
 
-map("", "<F2>", '', {
+map("", "<F2>", "", {
   noremap = true,
   silent = true,
-  callback = function ()
+  callback = function()
     toggle_mouse_and_fold()
   end,
-  desc = "toggle both mouse mode and fold (enable/disable)"
+  desc = "toggle both mouse mode and fold (enable/disable)",
 })
+
+-- clear 'n', 'N' map set by layzvim, act as default
+vim.keymap.del("n", "n")
+vim.keymap.del("o", "n")
+vim.keymap.del("x", "n")
+vim.keymap.del("n", "N")
+vim.keymap.del("o", "N")
+vim.keymap.del("x", "N")

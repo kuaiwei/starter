0cc375baf7041e458aa58a23e397e6c1966db77c
Kuai Wei <kuaiwei.kw@alibaba-inc.com>
2026-09-03 15:18:48 +0800

    use osc-52 clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -34,3 +34,17 @@ end
 
 -- disable autoformat
 vim.g.autoformat = false
+
+-- clipboard
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  }
+}
+
